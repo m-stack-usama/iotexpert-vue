c6a98101c2093477c1194f625980e6beab894adb
usamanosherwan <usamanosherwan729@gmail.com>
2025-01-16 15:36:20 +0500

    TailwindCss added

--- src/App.vue
+++ src/App.vue
@@ -1,101 +1,21 @@
 <script setup>
 // import { RouterLink, RouterView } from 'vue-router'
-// import HelloWorld from './components/HelloWorld.vue'
-
-import Button from "primevue/button";
-
-import InputText from "primevue/inputtext";
-
 import { ref } from "vue";
-
+import InputText from "primevue/inputtext";
 const value = ref(null);
 </script>
 
 <template>
-  <span>Hello Primevue</span>
-
-  <div class="card flex justify-center">
-    <InputText type="text" v-model="value" />
-    <Button label="Submit" />
+  <div
+    class="card w-full h-screen flex flex-col items-center justify-center space-y-5"
+  >
+    <h1 class="text-2xl tracking-wider underline mt-10">
+      Say Hello to PrimeVue & TailwindCss!
+    </h1>
+    <InputText type="text" v-model="value" placeholder="Say something!" />
+    {{ value }}
   </div>
-  {{ value }}
-
-  <!-- <header>
-    <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" />
-
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-
-      <nav>
-        <RouterLink to="/">Home</RouterLink>
-        <RouterLink to="/about">About</RouterLink>
-      </nav>
-    </div>
-  </header> 
-
-  <RouterView />-->
+  <!--<RouterView /> -->
 </template>
 
-<style scoped>
-header {
-  line-height: 1.5;
-  max-height: 100vh;
-}
-
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
-
-nav {
-  width: 100%;
-  font-size: 12px;
-  text-align: center;
-  margin-top: 2rem;
-}
-
-nav a.router-link-exact-active {
-  color: var(--color-text);
-}
-
-nav a.router-link-exact-active:hover {
-  background-color: transparent;
-}
-
-nav a {
-  display: inline-block;
-  padding: 0 1rem;
-  border-left: 1px solid var(--color-border);
-}
-
-nav a:first-of-type {
-  border: 0;
-}
-
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
-
-  nav {
-    text-align: left;
-    margin-left: -1rem;
-    font-size: 1rem;
-
-    padding: 1rem 0;
-    margin-top: 1rem;
-  }
-}
-</style>
+<style scoped></style>
--- src/main.js
+++ src/main.js
@@ -1,6 +1,5 @@
-import "./assets/main.css";
-
 import { createApp } from "vue";
+import "./assets/style.css";
 import App from "./App.vue";
 import router from "./router";
 import PrimeVue from "primevue/config";
